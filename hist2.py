ebdca15383f989d2e08f01d383dae25556933547
Jay Chan <crookedjustice@hotmail.com>
2017-06-14 23:43:43 -0700

    remove relience on site for hist2

--- hist2.py
+++ hist2.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python -S
+
 import os, sys, tty, termios, re
-from PIL import Image, ImageDraw
-import cStringIO as StringIO
 import base64
+import random
+from struct import pack
+
 
 def get_cell_size():
     termios.tcflush(0, termios.TCIOFLUSH)
@@ -29,13 +32,45 @@ def get_window_size():
     return int(int(w) * c_w), int(int(h) * c_h)
 
 
+class PAM(object):
+    def __init__(self, size):
+        self.w, self.h = size
+
+        self.data = [(0, 0, 0, 0)] * (self.w * self.h)
+
+    def rect(self, pos, fill=(255, 0, 0, 0)):
+        (a, b), (c, d) = pos
+        if c < a:
+            a, c = c, a
+        if d < b:
+            b, d = d, b
+
+        for i in xrange(a, c + 1):
+            for j in xrange(b, d + 1):
+                self.data[j * self.w + i] = fill
+
+    def as_string(self):
+        s = """P7
+WIDTH %d
+HEIGHT %d
+DEPTH 4
+MAXVAL 255
+TUPLTYPE RGB_ALPHA
+ENDHDR\n""" % (self.w, self.h)
+
+        for rgba in self.data:
+            s += pack("BBBB", *rgba)
+
+        return s
+
+
 class Plot(object):
     def __init__(self, N, Z=3, rows=1):
         w, h = map(int, get_cell_size())
         h *= rows
         self.size = (w, h)
         self.Z = Z
-        self.img = Image.new('RGBA', (Z * N, h))
+        self.pam = PAM((Z*N, h))
 
     def line_plot(self, data, color=(0, 255, 255, 255)):
         w, h = self.size
@@ -58,7 +93,6 @@ class Plot(object):
         M = max(data) - m
         Z = self.Z
 
-        draw = ImageDraw.Draw(self.img)
         for i, v in enumerate(data):
             v = float(v - m) / M
             p = int(v * h)
@@ -69,7 +103,7 @@ class Plot(object):
             else:
                 k = color
 
-            draw.rectangle([
+            self.pam.rect([
                 (Z * i + 1, h - 1),
                 (Z * i + Z - 1, h - p - 1),
             ], fill=k)
@@ -83,36 +117,43 @@ class Plot(object):
 
         base = h / 2
 
-        draw = ImageDraw.Draw(self.img)
         for i, v in enumerate(data):
             v = float(v) / M / 2
             p = int(v * h)
             c = int((v + .5) * 255)
 
-            draw.rectangle([
+            self.pam.rect([
                 (Z * i + 1, h - 1 - base),
                 (Z * i + Z - 1, h - 1 - p - base),
             ], fill=(255 - c, c, 0, 255))
 
     def output(self):
-        buff = StringIO.StringIO()
-        self.img.save(buff, format='PNG')
-
         sys.stdout.write("\x1b]1337;File=inline=1:%s\a" % (
-            base64.b64encode(buff.getvalue()),
+            base64.b64encode(self.pam.as_string()),
         ))
         sys.stdout.flush()
 
-import random
-N=100
-#dat = [i for i in xrange(N)]
-dat = [random.random() * 2 - 1 for i in xrange(N)]
-#dat = [(i - 50.) / 100 for i in xrange(N)]
+def main():
+    N=100
+    #dat = [i for i in xrange(N)]
+    dat = [random.random() * 2 - 1 for i in xrange(N)]
+    #dat = [(i - 50.) / 100 for i in xrange(N)]
 
-plt = Plot(N, Z=3, rows=1)
-#plt.line_plot(dat)
-#plt.bar_plot(dat)
-plt.center_plot(dat)
-plt.output()
+    plt = Plot(N, Z=5, rows=1)
+    #plt.line_plot(dat)
+    #plt.bar_plot(dat)
+    plt.center_plot(dat)
+    plt.output()
 
-print ''
+    print ''
+
+#import cStringIO, cProfile, pstats
+#pr = cProfile.Profile()
+#pr.enable()
+main()
+#pr.disable()
+#s = cStringIO.StringIO()
+#sortby = 'cumulative'
+#ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+#ps.print_stats()
+#print s.getvalue()
